9be14717587d3aac69d2bea66b729303b9058151
Buram Sri Sai rishika <rishikaburam25@gmail.com>
2025-11-05 19:07:45 +0530

    Change file upload check to stop on None

--- IPL match (2).py
+++ IPL match (2).py
@@ -16,8 +16,8 @@ st.title("IPL Data Analysis Dashboard")
 # Load the dataset
 data = pd.read_csv("matches(1).csv")
 uploaded_file = st.file_uploader("Upload your IPL CSV file", type="csv")
-if uploaded_file is not None:
-    st.success("File uploaded successfully!")
+if uploaded_file is None:
+    st.stop()
 
 
 
